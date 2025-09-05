03ad3fd3c3313e34788eb5537b6c6d7d37c210c6
alireza <alirezaabdolmaleki600@gmail.com>
2025-09-05 12:43:05 +0330

    remove .env file and make goodenv optional

--- main.go
+++ main.go
@@ -18,9 +18,7 @@ import (
 
 func main() {
 	// Load environment
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file:", err)
-	}
+	_ = godotenv.Load()
 
 	token := os.Getenv("TELEGRAM_BOT_TOKEN")
 	supaBaseUrl := os.Getenv("SUPABASE_URL")
